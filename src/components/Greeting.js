0a090adb2057eb9c19ad5221ffc0c30b7558dd99
Nahom <zerihunnahom@gmail.com>
2023-07-13 15:35:43 +0300

    Get greeting from the redux store and use it

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from '../redux/features/greeting/greetingSlice';
+
 const Greeting = () => {
+  const { greeting } = useSelector((store) => store.greeting);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, [dispatch]);
+
   return (
-    <h1>Hello</h1>
+    <h1>{greeting.title}</h1>
   );
 };
 export default Greeting;
